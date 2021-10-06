482c054587f9a9f2bb48c4dfe05598adb5c32a91
EC2 Default User <ec2-user@ip-172-31-82-122.ec2.internal>
2021-10-06 20:26:01 +0000

    part 1 complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,12 +4,14 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
-  Movie.count.should be n_seeds.to_i
+  #Movie.count.should be n_seeds.to_i   #deprecated
+  expect(Movie.count).to eq n_seeds.to_i
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -18,7 +20,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  expect(page.body.index(e1)<page.body.index(e2))
+  #fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +32,22 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(',').each do |ratings|
+    When /^(?:|I )check "([^"]*)"$/ do |field|
+      check(field)
+    end
+
+    When /^(?:|I )uncheck "([^"]*)"$/ do |field|
+      uncheck(field)
+    end
+  end
+  #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  Movie.all.each do |movie|
+    step "I should see #{movie.title}"
+  end
+  #fail "Unimplemented"
 end
